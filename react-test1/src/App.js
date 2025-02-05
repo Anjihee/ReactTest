adb28bde9091bc48965b41fcce36ccca87e67c6a
Jihee <115629285+Anjihee@users.noreply.github.com>
2025-02-05 19:28:11 +0900

    Include react-test1 inside ReactStudy repository

---
+++ react-test1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React! 🚀</h1>
+      <p>이것은 내가 수정한 첫 번째 React 페이지입니다!</p>
+    </div>
+  );
+}
+
+export default App;
